86edd7ae39572f5f8487d647d1c402c8ed09256c
Hardik Rana <ranahardik2207@gmail.com>
2022-04-25 09:07:19 -0700

    Error solved

--- src/components/Log/LogItem.js
+++ src/components/Log/LogItem.js
@@ -3,13 +3,17 @@ import React from 'react'
 
 const Log = ({log}) => {
 
+      console.log(typeof(log.ATTENTION));
+      console.log(log.LOG)
 
     return (
       <ul className="list-group list-group-flush">
       <li className="list-group-item">
       <div>
-        <a href="#edit-log-modal" className="delete_link">{log.LOG}</a>
+        <a href="#edit-log-modal" className={`delete_link ${log.ATTENTION === "1" ? 'text-danger' : 'text-primary'}`}>{log.LOG}</a>
         <br/>
+
+       
         <span className='grey-text'>
             <span className="black-text">ID #{log.ID}</span> last updated by {'  '}<span className="black-text">{log.USERNAME}</span> on {log.DATE}
         </span>
